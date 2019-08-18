f55a9284efbce9cbfb7f498265e90466f37c2318
sk2628 <steve.kang@iapplytics.com>
2019-08-18 09:51:35 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -40,11 +40,11 @@ var checkForMatch = function(){
     }
     else
     {
-        console.log("Sorry. Try Again");
+        alert("Sorry. Try Again");
     }
 }
 
-var flipCard = function(cardId){
+var flipCard = function(){
     //var cardOne = cards[0];
     //cardsInPlay.push(cardOne);
 
@@ -54,16 +54,31 @@ var flipCard = function(cardId){
    // cardsInPlay.push(cardTwo);
 
     //console.log("User flipped " + cardsInPlay);
+    var cardId = this.getAttribute('data-id');
+
     console.log("User flipped " + cards[cardId].rank);
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
     cardsInPlay.push(cards[cardId].rank);
 
+    this.setAttribute('src', cards[cardId].cardImage);
     if(cardsInPlay.length === 2)
     {
         checkForMatch();
     }
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+    for (var i = 0; i < cards.length; i++){
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+}
+
+createBoard();
+//flipCard(0);
+//flipCard(2);
